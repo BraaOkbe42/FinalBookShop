25b27a04210ce12949e32ba37006e150cf4da5cc
Braa Okbe <117113313+BraaOkbe42@users.noreply.github.com>
2024-03-01 14:19:16 +0200

    Add project files.

---
+++ Models/CartItem.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.Models
+{
+    public class CartItem
+    {
+        [Key]
+        public int CartItemID { get; set; }
+
+        [ForeignKey("Cart")]
+        public int CartID { get; set; }
+
+        [ForeignKey("Book")]
+        public int BookID { get; set; }
+        public int Quantity { get; set; }
+
+        public Cart Cart { get; set; }
+        public Book Book { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+using BookShop.Data;
+using BookStore.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BookStoreContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+if (args.Length == 1 && args[0].ToLower() == "seeddata")
+{
+    //await Seed.SeedUsersAndRolesAsync(app);
+    Seed.SeedData(app);
+}
+
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
